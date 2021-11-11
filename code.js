3b93ace750c9c552850e19376fc5441dc76dd254
Blazer <51216880+BlaizerBrumo@users.noreply.github.com>
2021-11-11 23:15:56 +0000

    Initial commit

---
+++ code.js
@@ -0,0 +1,26 @@
+
+
+
+
+var title = localStorage.getItem("title")
+
+if (localStorage.hasOwnProperty("title")) {
+    document.title = title
+}
+
+
+
+function settitle(title) {
+  if (title !== "") {
+  localStorage.setItem("title", title)
+  document.title = title
+  } else {
+  localStorage.removeItem("title")
+  document.title = "\u200E"
+  }
+}
+
+function setdefault() {
+  settitle("School Heaven")
+}
+
